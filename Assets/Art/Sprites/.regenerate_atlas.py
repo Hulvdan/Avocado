0b3ccd8e1cccadfd9688c39f297f4ac3d79165e8
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-09-22 00:46:36 +0700

    Fixed atlas

--- Assets/Art/Sprites/.regenerate_atlas.py
+++ Assets/Art/Sprites/.regenerate_atlas.py
@@ -2,30 +2,37 @@ import os
 
 
 def regenerate_atlas():
-  print("1. Regenerating atlas...")
-  os.system(" ".join(["free-tex-packer-cli", "--project", ".atlas.ftpp"]))
+    print("1. Regenerating atlas...")
+    os.system(" ".join(["free-tex-packer-cli", "--project", ".atlas.ftpp"]))
 
 
 def update_atlas_data():
-  print("2. Updating atlas data...")
-  with open("atlas.tpsheet") as in_file:
-    data = in_file.readlines()
+    print("2. Updating atlas data...")
+    with open("atlas.tpsheet") as in_file:
+        data = in_file.readlines()
 
-  updated_lines = []
-  for line in data:
-    if not len(line):
-      updated_lines.append(line)
-      continue
+    beginning_lines = []
+    updated_lines = []
 
-    if line[0] == ".":
-      line = line.split("-", 1)[-1]
-    updated_lines.append(line)
+    for line in data:
+        if not len(line):
+            updated_lines.append(line)
+            continue
 
-  with open("atlas.tpsheet", "w") as out_file:
-    out_file.write("".join(updated_lines))
+        if line[0] == ".":
+            line = line.split("-", 1)[-1]
+            updated_lines.append(line)
+        else:
+            beginning_lines.append(line)
+
+    updated_lines.sort()
+
+    with open("atlas.tpsheet", "w") as out_file:
+        out_file.write("".join(beginning_lines))
+        out_file.write("".join(updated_lines))
 
 
 if __name__ == "__main__":
-  regenerate_atlas()
-  update_atlas_data()
-  input("Press Enter to exit. ")
+    regenerate_atlas()
+    update_atlas_data()
+    input("Press Enter to exit. ")
