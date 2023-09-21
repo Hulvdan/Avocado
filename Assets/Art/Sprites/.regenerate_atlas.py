992aee4be8c2e0e4808db6bba731c04a35169201
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-09-21 23:44:20 +0700

    Atlas generation

---
+++ Assets/Art/Sprites/.regenerate_atlas.py
@@ -0,0 +1,31 @@
+import os
+
+
+def regenerate_atlas():
+  print("1. Regenerating atlas...")
+  os.system(" ".join(["free-tex-packer-cli", "--project", ".atlas.ftpp"]))
+
+
+def update_atlas_data():
+  print("2. Updating atlas data...")
+  with open("atlas.tpsheet") as in_file:
+    data = in_file.readlines()
+
+  updated_lines = []
+  for line in data:
+    if not len(line):
+      updated_lines.append(line)
+      continue
+
+    if line[0] == ".":
+      line = line.split("-", 1)[-1]
+    updated_lines.append(line)
+
+  with open("atlas.tpsheet", "w") as out_file:
+    out_file.write("".join(updated_lines))
+
+
+if __name__ == "__main__":
+  regenerate_atlas()
+  update_atlas_data()
+  input("Press Enter to exit. ")
