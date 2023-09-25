39c7e36c79b17fdb70c89de16bf6a7e0e27abc5d
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-09-26 02:23:02 +0700

    Horizontal camera dead zone

--- Assets/Scripts/Runtime/BFG/Avocado/BetterCamera.cs
+++ Assets/Scripts/Runtime/BFG/Avocado/BetterCamera.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 
 namespace BFG.Avocado {
+[RequireComponent(typeof(Camera))]
 public class BetterCamera : MonoBehaviour {
     [SerializeField]
     AvocadoController controller;
@@ -9,20 +10,47 @@ public class BetterCamera : MonoBehaviour {
     [Min(0)]
     float followingSpeed = 1f;
 
-    Transform _character;
+    [SerializeField]
+    [Min(0)]
+    float horizontalDeadZone = 1f;
+
+    Transform _avocado;
+    Camera _camera;
 
     void Start() {
-        _character = controller.transform;
+        _camera = GetComponent<Camera>();
+        _avocado = controller.transform;
     }
 
     void LateUpdate() {
         var k = followingSpeed * Time.deltaTime;
         var pos = transform.position;
-        var charPos = _character.position;
-        transform.position = new Vector3(
-            Mathf.Lerp(pos.x, charPos.x, k),
-            Mathf.Lerp(pos.y, charPos.y, k),
-            pos.z
+        var charPos = _avocado.position;
+
+        var x = pos.x;
+        if (charPos.x > pos.x + horizontalDeadZone) {
+            x = Mathf.Lerp(pos.x + horizontalDeadZone, charPos.x, k) - horizontalDeadZone;
+        }
+        else if (charPos.x < pos.x - horizontalDeadZone) {
+            x = Mathf.Lerp(pos.x - horizontalDeadZone, charPos.x, k) + horizontalDeadZone;
+        }
+
+        transform.position = new Vector3(x, Mathf.Lerp(pos.y, charPos.y, k), pos.z);
+    }
+
+    void OnDrawGizmos() {
+        _camera = GetComponent<Camera>();
+
+        Gizmos.color = Color.cyan;
+        var pos = transform.position;
+        var size = _camera.orthographicSize;
+        Gizmos.DrawLine(
+            new Vector3(pos.x - horizontalDeadZone, pos.y - size, pos.z),
+            new Vector3(pos.x - horizontalDeadZone, pos.y + size, pos.z)
+        );
+        Gizmos.DrawLine(
+            new Vector3(pos.x + horizontalDeadZone, pos.y - size, pos.z),
+            new Vector3(pos.x + horizontalDeadZone, pos.y + size, pos.z)
         );
     }
 }
